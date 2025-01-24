f04fde577d583f4c74b78f7898448ca367039697
mahon479 <mahon479@morris.umn.edu>
2025-01-24 10:40:50 -0600

    Completed semantics2

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -21,7 +21,7 @@ mod tests {
     fn move_semantics2() {
         let mut vec0 = vec![22, 44, 66];
 
-        let mut vec1 = fill_vec(vec0);
+        let mut vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
